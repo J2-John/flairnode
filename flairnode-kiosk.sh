2416b06edec7f0167d749e4c06352a05b9e23f27
John the Boss <john@jsquaredsystems.com>
2026-06-30 15:49:18 -0500

    Fix kiosk script: add missing backslash after --kiosk flag, add background stability flags

--- flairnode-kiosk.sh
+++ flairnode-kiosk.sh
@@ -6,10 +6,17 @@ echo "Starting Chromium test kiosk..."
 sleep 3;
 
 chromium \
-        --kiosk file:///home/user/flairnode/render.html
+        --kiosk file:///home/user/flairnode/render.html \
         --noerrdialogs \
         --disable-infobars \
         --disable-session-crashed-bubble \
         --no-first-run \
         --disable-features=PointerLock \
+        --disable-translate \
+        --disable-component-update \
+        --disable-background-networking \
+        --disable-sync \
+        --metrics-recording-only \
+        --no-service-autorun \
+        --autoplay-policy=no-user-gesture-required \
         --user-data-dir="$HOME/.flair-chrome-test"
